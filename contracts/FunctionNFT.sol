5701510fb7ecd4c5e26e5ecf15f1b46cf526bd67
Duy Huynh <duy.huynh.uiv@gmail.com>
2022-09-20 11:14:32 +0700

    add withdraw function

--- contracts/FunctionNFT.sol
+++ contracts/FunctionNFT.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.6;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
@@ -24,12 +25,16 @@ contract FunctionalNFT is ERC721URIStorage, ReentrancyGuard, Ownable {
         // the specific value of a param (attribute) of an NFT
         mapping(uint256 => mapping(string => string)) private _values;
 
-        constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
+        // minting fee, if any
+        uint256 private _fee;
+
+        constructor(string memory name, string memory symbol, uint256 fee) ERC721(name, symbol) {
+                _fee = fee;
+        }
 
         function setCode(string memory code) public onlyOwner {
                 _code = code;
         }
-
 
         function setRange(string memory param, string[] memory range) public onlyOwner {
                 _ranges[param] = range;
@@ -44,20 +49,22 @@ contract FunctionalNFT is ERC721URIStorage, ReentrancyGuard, Ownable {
                 return _code;
         }
 
-        // TODO: look at common format so it can be displayed on opensea etc
-        function getValue(uint256 id, string memory param) public view returns (string memory) {
-                return _values[id][param];
-        }
+        function mint() public nonReentrant payable returns (uint256) {
 
-        function mint() public nonReentrant returns (uint256) {
+                require(msg.value >= _fee);
+
                 uint256 id = _counter.current();
+
+                _randomize(id);
+                _setTokenURI(id, "TODO"); // TODO: link to the metadata that opensea can uderstand
+
                 _mint(msg.sender, id);
-                _setTokenURI(id, _randomAttributes(id));
+
                 _counter.increment();
                 return id;
         }
 
-        function _randomAttributes(uint256 id) internal returns (string memory) {
+        function _randomize(uint256 id) internal returns (string memory) {
                 string memory content;
 
                 for (uint256 i = 0; i < _params.length; i++) {
@@ -73,5 +80,10 @@ contract FunctionalNFT is ERC721URIStorage, ReentrancyGuard, Ownable {
 
         function _randomNumber(uint256 n) internal view returns (uint256) {
                 return uint(keccak256(abi.encodePacked(block.difficulty, msg.sender, block.timestamp))) % n;
+        }
+
+        // TODO: test this one carefully
+        function withdraw() public onlyOwner {
+                payable(msg.sender).transfer(address(this).balance);
         }
 }
