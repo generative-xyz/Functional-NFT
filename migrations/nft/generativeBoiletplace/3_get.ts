699d2ffecb62ee82dfb1c4dce12a1badee55878e
thaibao-incognito <thaibao@autonomous.nyc>
2022-10-06 12:52:52 +0700

    - update script

--- migrations/nft/generativeBoiletplace/3_get.ts
+++ migrations/nft/generativeBoiletplace/3_get.ts
@@ -11,17 +11,17 @@ import {GenerativeBoilerplateNFT} from "./GenerativeBoilerplateNFT";
         }
         const contract = '0x19cbe1721a63dd4f391fc6f0a75596fe98c2301a';
         const nft = new GenerativeBoilerplateNFT(process.env.NETWORK, process.env.PRIVATE_KEY, process.env.PUBLIC_KEY);
-        const tokenId = 1;
+        const projectId = 1;
         let a: any = {};
-        // a.getTokenURI = await nft.getTokenURI(contract, tokenId);
-        // a.getScript = await nft.getScript(contract, tokenId);
-        // a.getScriptType = await nft.getScriptType(contract, tokenId);
-        // a.getFee = await nft.getFee(contract, tokenId);
-        // a.getFeeTokens = await nft.getFeeTokens(contract, tokenId);
-        // a.getNFTContract = await nft.getNFTContract(contract, tokenId);
-        // a.get_mintTotalSupply = await nft.get_mintTotalSupply(contract, tokenId);
-        // a.get_mintMaxSupply = await nft.get_mintMaxSupply(contract, tokenId);
-        a.get_nftContracts = await nft.get_nftContracts(contract, process.env.PUBLIC_KEY, tokenId);
+        // a.getTokenURI = await nft.getTokenURI(contract, projectId);
+        // a.getScript = await nft.getScript(contract, projectId);
+        // a.getScriptType = await nft.getScriptType(contract, projectId);
+        // a.getFee = await nft.getFee(contract, projectId);
+        // a.getFeeTokens = await nft.getFeeTokens(contract, projectId);
+        // a.getNFTContract = await nft.getNFTContract(contract, projectId);
+        a.get_mintTotalSupply = await nft.get_mintTotalSupply(contract, projectId);
+        a.get_mintMaxSupply = await nft.get_mintMaxSupply(contract, projectId);
+        a.get_nftContracts = await nft.get_nftContracts(contract, process.env.PUBLIC_KEY, projectId);
         console.log({a});
     } catch (e) {
         // Deal with the fact the chain failed
