f590b33b725a09375ae18f349fb649be1ba66491
thaibao-incognito <thaibao@autonomous.nyc>
2022-10-13 09:19:31 +0700

    - optimize code

--- contracts/governance/ParameterControl.sol
+++ contracts/governance/ParameterControl.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.12;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "../interfaces/IParameterControl.sol";
+import "../lib/helpers/Errors.sol";
 
 /*
  * @dev Implementation of a programmable parameter control.
@@ -25,9 +26,14 @@ contract ParameterControl is AccessControl, IParameterControl {
     constructor(
         address admin_
     ) {
-        require(admin_ != address(0x0), "admin is zero address");
+        require(admin_ != address(0x0), Errors.INV_ADD);
         admin = admin_;
         _setupRole(DEFAULT_ADMIN_ROLE, admin);
+    }
+
+    modifier adminOnly() {
+        require(msg.sender == admin && hasRole(DEFAULT_ADMIN_ROLE, msg.sender), Errors.ONLY_ADMIN_ALLOWED);
+        _;
     }
 
     function get(string memory key) external view returns (string memory) {
@@ -46,35 +52,25 @@ contract ParameterControl is AccessControl, IParameterControl {
         return _paramsAddress[key];
     }
 
-    function set(string memory key, string memory value) external {
-        require(msg.sender == admin, "Sender is not admin");
-        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Caller is not a admin");
+    function set(string memory key, string memory value) external adminOnly {
         _params[key] = value;
         emit SetEvent(key, value);
     }
 
-    function setInt(string memory key, int value) external {
-        require(msg.sender == admin, "Sender is not admin");
-        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Caller is not a admin");
+    function setInt(string memory key, int value) external adminOnly {
         _paramsInt[key] = value;
     }
 
-    function setUInt256(string memory key, uint256 value) external {
-        require(msg.sender == admin, "Sender is not admin");
-        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Caller is not a admin");
+    function setUInt256(string memory key, uint256 value) external adminOnly {
         _paramsUInt256[key] = value;
     }
 
-    function setAddress(string memory key, address value) external {
-        require(msg.sender == admin, "Sender is not admin");
-        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Caller is not a admin");
+    function setAddress(string memory key, address value) external adminOnly {
         _paramsAddress[key] = value;
     }
 
-    function updateAdmin(address admin_) external {
-        require(msg.sender == admin);
-        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Caller is not a admin");
-        require(admin_ != address(0x0), "admin is zero address");
+    function updateAdmin(address admin_) external adminOnly {
+        require(admin_ != address(0x0), Errors.INV_ADD);
 
         address previousAdmin = admin;
         admin = admin_;
