bf0d26a59b8deaf159b412f211699d0e08034e5e
thaibao-incognito <thaibao@autonomous.nyc>
2022-10-18 10:14:35 +0700

    - update script

--- migrations/nft/generativeBoiletplace/1.2_upgradeContract.ts
+++ migrations/nft/generativeBoiletplace/1.2_upgradeContract.ts
@@ -8,7 +8,7 @@ import {GenerativeBoilerplateNFT} from "./GenerativeBoilerplateNFT";
         }
 
         const nft = new GenerativeBoilerplateNFT(process.env.NETWORK, process.env.PRIVATE_KEY, process.env.PUBLIC_KEY);
-        const address = await nft.upgradeContract("0xae0C96BBD7733a1C7843af27e0683c74E182A3a7");
+        const address = await nft.upgradeContract("0x95056d48b6DC390304e6d5c4638A413E7Bd931ce");
         console.log({address});
     } catch (e) {
         // Deal with the fact the chain failed
