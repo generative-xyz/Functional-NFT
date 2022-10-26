021ba8f454c09a21afc5084f01bc5c57e2b78829
thaibao-incognito <thaibao@autonomous.nyc>
2022-10-26 23:45:43 +0700

    - update script

--- migrations/nft/generativenft/3_get.ts
+++ migrations/nft/generativenft/3_get.ts
@@ -9,15 +9,17 @@ import {GeneretiveNFT} from "./GeneretiveNFT";
             console.log("wrong network");
             return;
         }
-        const contract = '0xBb96facC454257B320e9a70Fd7905bb159FC9981';
+        const contract = '0xc8CB5439c767A63aca1c01862252B2F3495fDcFE';
         const nft = new GeneretiveNFT(process.env.NETWORK, process.env.PRIVATE_KEY, process.env.PUBLIC_KEY);
         const tokenId = 1;
         let a: any = {};
         // a.getTokenURI = await nft.getTokenURI(contract, tokenId);
         // a.get_boilerplateAdd = await nft.get_boilerplateAddr(contract);
         // a.get_boilerplateId = await nft.get_boilerplateId(contract);
-        a.get_paramsValues = await nft.get_paramsValues(contract, tokenId);
-        console.log(a.get_paramsValues);
+        // a.get_paramsValues = await nft.get_paramsValues(contract, tokenId);
+        // a.getTraits = await nft.getTraits(contract);
+        a.getTokenTraits = await nft.getTokenTraits(contract, tokenId);
+        console.log(a.getTokenTraits);
     } catch (e) {
         // Deal with the fact the chain failed
         console.log(e);
--- migrations/nft/generativenft/GeneretiveNFT.ts
+++ migrations/nft/generativenft/GeneretiveNFT.ts
@@ -104,6 +104,84 @@ class GeneretiveNFT {
         const val: any = await temp?.nftContract.methods.getParamValues(tokenId).call(tx);
         return val;
     }
+
+    async getTraits(contractAddress: any) {
+        let temp = this.getContract(contractAddress);
+        const nonce = await temp?.web3.eth.getTransactionCount(this.senderPublicKey, "latest") //get latest nonce
+        console.log("asfafaf");
+        //the transaction
+        const tx = {
+            from: this.senderPublicKey,
+            to: contractAddress,
+            nonce: nonce,
+        }
+
+
+        const val: any = await temp?.nftContract.methods.getTraits().call(tx);
+        return val;
+    }
+
+    async getTokenTraits(contractAddress: any, tokenId: number) {
+        let temp = this.getContract(contractAddress);
+        const nonce = await temp?.web3.eth.getTransactionCount(this.senderPublicKey, "latest") //get latest nonce
+        //the transaction
+        const tx = {
+            from: this.senderPublicKey,
+            to: contractAddress,
+            nonce: nonce,
+        }
+
+
+        const val: any = await temp?.nftContract.methods.getTokenTraits(tokenId).call(tx);
+        return val;
+    }
+
+    async updateTraits(contractAddress: any, traits: any,
+                       gas: any) {
+        let temp = this.getContract(contractAddress);
+        const nonce = await temp?.web3.eth.getTransactionCount(this.senderPublicKey, "latest") //get latest nonce
+
+        const fun = temp?.nftContract.methods.updateTraits(traits);
+        //the transaction
+        const tx = {
+            from: this.senderPublicKey,
+            to: contractAddress,
+            nonce: nonce,
+            gas: gas,
+            data: fun.encodeABI(),
+        }
+
+        if (tx.gas == 0) {
+            tx.gas = await fun.estimateGas(tx);
+        }
+
+        return await this.signedAndSendTx(temp?.web3, tx);
+    }
+
+    async signedAndSendTx(web3: any, tx: any) {
+        const signedTx = await web3.eth.accounts.signTransaction(tx, this.senderPrivateKey)
+        if (signedTx.rawTransaction != null) {
+            let sentTx = await web3.eth.sendSignedTransaction(
+                signedTx.rawTransaction,
+                function (err: any, hash: any) {
+                    if (!err) {
+                        console.log(
+                            "The hash of your transaction is: ",
+                            hash,
+                            "\nCheck Alchemy's Mempool to view the status of your transaction!"
+                        )
+                    } else {
+                        console.log(
+                            "Something went wrong when submitting your transaction:",
+                            err
+                        )
+                    }
+                }
+            )
+            return sentTx;
+        }
+        return null;
+    }
 }
 
 export {GeneretiveNFT};
