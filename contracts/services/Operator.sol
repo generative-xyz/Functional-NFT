714545e8196868ef2ddc184294586aa59e7a288d
thaibao-incognito <thaibao@autonomous.nyc>
2022-11-14 10:10:43 +0700

    - update

--- contracts/services/Operator.sol
+++ contracts/services/Operator.sol
@@ -1,4 +1,9 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// uncomment and deploy by https://remix.ethereum.org/
+/*
 pragma solidity ^0.7.0;
 
 import "@chainlink/contracts/src/v0.7/Operator.sol";
+*/
