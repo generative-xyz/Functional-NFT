1ae17609584a50753f42ff2a4e3b0144290649c5
thaibao-incognito <thaibao@autonomous.nyc>
2022-11-16 15:08:01 +0700

    - add more script

---
+++ migrations/nft/avatars/2.12_pause.ts
@@ -0,0 +1,23 @@
+import * as dotenv from 'dotenv';
+
+import {ethers} from "ethers";
+import * as fs from "fs";
+import {keccak256} from "ethers/lib/utils";
+import {AVATARS} from "./avatars";
+
+(async () => {
+    try {
+        if (process.env.NETWORK != "mumbai") {
+            console.log("wrong network");
+            return;
+        }
+        const contract = '0x9e339b1b85b00feffa44b1a120702c5a40935391';
+        const nft = new AVATARS(process.env.NETWORK, process.env.PRIVATE_KEY, process.env.PUBLIC_KEY);
+
+        const tx = await nft.pause(contract, 0);
+        console.log("tx:", tx);
+    } catch (e) {
+        // Deal with the fact the chain failed
+        console.log(e);
+    }
+})();
--- migrations/nft/avatars/avatars.ts
+++ migrations/nft/avatars/avatars.ts
@@ -300,6 +300,48 @@ class AVATARS {
         return await this.signedAndSendTx(temp?.web3, tx);
     }
 
+    async pause(contractAddress: any, gas: number) {
+        let temp = this.getContract(contractAddress);
+        const nonce = await temp?.web3.eth.getTransactionCount(this.senderPublicKey, "latest") //get latest nonce
+
+        const fun = temp?.nftContract.methods.pause();
+        //the transaction
+        const tx = {
+            from: this.senderPublicKey,
+            to: contractAddress,
+            nonce: nonce,
+            gas: gas,
+            data: fun.encodeABI(),
+        }
+
+        if (tx.gas == 0) {
+            tx.gas = await fun.estimateGas(tx);
+        }
+
+        return await this.signedAndSendTx(temp?.web3, tx);
+    }
+
+    async unpause(contractAddress: any, gas: number) {
+        let temp = this.getContract(contractAddress);
+        const nonce = await temp?.web3.eth.getTransactionCount(this.senderPublicKey, "latest") //get latest nonce
+
+        const fun = temp?.nftContract.methods.unpause();
+        //the transaction
+        const tx = {
+            from: this.senderPublicKey,
+            to: contractAddress,
+            nonce: nonce,
+            gas: gas,
+            data: fun.encodeABI(),
+        }
+
+        if (tx.gas == 0) {
+            tx.gas = await fun.estimateGas(tx);
+        }
+
+        return await this.signedAndSendTx(temp?.web3, tx);
+    }
+
     async mint(contractAddress: any, fee: any, gas: number) {
         let temp = this.getContract(contractAddress);
         const nonce = await temp?.web3.eth.getTransactionCount(this.senderPublicKey, "latest") //get latest nonce
