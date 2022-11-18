0a4d316a8eeaab783c920d4d1f648ac611194564
thaibao-incognito <thaibao@autonomous.nyc>
2022-11-18 08:57:10 +0700

    - add more script

---
+++ migrations/nft/sweet/4.1_getParamsValue.ts
@@ -0,0 +1,31 @@
+import * as dotenv from 'dotenv';
+
+import {ethers} from "ethers";
+import * as fs from "fs";
+import {keccak256} from "ethers/lib/utils";
+import {SWEETS} from "./sweets";
+
+(async () => {
+    try {
+        if (process.env.NETWORK != "mainnet") {
+            console.log("wrong network");
+            return;
+        }
+        const contract = '0x57425e74a8d9b09a4e1a713d727b05084fddef12';
+        const nft = new SWEETS(process.env.NETWORK, process.env.PRIVATE_KEY, process.env.PUBLIC_KEY);
+        let count = 0;
+        let tokenIds = [];
+        for (var i = 4526; i <= 5000; i++) {
+            const a = await nft.getParamValues(contract, i);
+            if (a.shape == "Pillhead") {
+                console.log(i, a);
+                tokenIds.push(i);
+                count++;
+            }
+        }
+        console.log(tokenIds, count);
+    } catch (e) {
+        // Deal with the fact the chain failed
+        console.log(e);
+    }
+})();
---
+++ migrations/nft/sweet/sweets.ts
@@ -0,0 +1,52 @@
+import {createAlchemyWeb3} from "@alch/alchemy-web3";
+import * as path from "path";
+import {Bytes32Ty} from "hardhat/internal/hardhat-network/stack-traces/logger";
+import {ethers as eth1} from "ethers";
+
+const {ethers, upgrades} = require("hardhat");
+const hardhatConfig = require("../../../hardhat.config");
+
+class SWEETS {
+    network: string;
+    senderPublicKey: string;
+    senderPrivateKey: string;
+
+    constructor(network: any, senderPrivateKey: any, senderPublicKey: any) {
+        this.network = network;
+        this.senderPrivateKey = senderPrivateKey;
+        this.senderPublicKey = senderPublicKey;
+    }
+
+    getContract(contractAddress: any, contractName: any = "./artifacts/contracts/nft/SWEETS.sol/SWEETS.json") {
+        console.log("Network run", this.network, hardhatConfig.networks[this.network].url);
+        if (this.network == "local") {
+            console.log("not run local");
+            return;
+        }
+        let API_URL: any;
+        API_URL = hardhatConfig.networks[hardhatConfig.defaultNetwork].url;
+
+        // load contract
+        let contract = require(path.resolve(contractName));
+        const web3 = createAlchemyWeb3(API_URL)
+        const nftContract = new web3.eth.Contract(contract.abi, contractAddress)
+        return {web3, nftContract};
+    }
+
+    async getParamValues(contractAddress: any, tokenId: any) {
+        let temp = this.getContract(contractAddress);
+        const nonce = await temp?.web3.eth.getTransactionCount(this.senderPublicKey, "latest") //get latest nonce
+
+        //the transaction
+        const tx = {
+            from: this.senderPublicKey,
+            to: contractAddress,
+            nonce: nonce,
+        }
+
+        const val: any = await temp?.nftContract.methods.getParamValues(tokenId).call(tx);
+        return val;
+    }
+}
+
+export {SWEETS};
